9c848fcba768aa763dba058638429172f6800516
yayaveli <yabadji2010@gmail.com>
2021-12-22 04:08:29 +0000

    Initial commit

---
+++ src/main/java/com/yayaveli/inventorymanagement/InventorymanagementApplication.java
@@ -0,0 +1,13 @@
+package com.yayaveli.inventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorymanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yayaveli/inventorymanagement/InventorymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yayaveli.inventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
